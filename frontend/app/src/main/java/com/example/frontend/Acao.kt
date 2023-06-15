00c79436dd8f83ce6c76e5c98cb885b483dc8fa0
kaiossoares <kaioantoniosoares@gmail.com>
2023-06-15 11:37:47 -0300

    Testing Gson on Kotlin

---
+++ frontend/app/src/main/java/com/example/frontend/Acao.kt
@@ -0,0 +1,12 @@
+package com.example.frontend
+
+class Acao {
+
+    var nome = ""
+    var tipo = ""
+    var descricao = ""
+    
+    override fun toString(): String {
+        return "Acao(nome='$nome', tipo='$tipo', descricao='$descricao')"
+    }
+}
--- frontend/app/src/main/java/com/example/frontend/MainActivity.kt
+++ frontend/app/src/main/java/com/example/frontend/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.frontend
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var acao = Acao()
+        acao.nome = "XPML11"
+        acao.tipo = "Fundo Imobiliário"
+        acao.descricao = "Fundo criado em 2017, sendo considerado um fundo de shopping."
+
+        var gson = Gson()
+
+        var acaoJson = gson.toJson(acao)
+
+        println("-------> " + acaoJson)
+
+        // Convertendo o json em um objeto Acao
+        var acao2 = Acao()
+        acao2 = gson.fromJson(acaoJson, Acao::class.java)
+
+        println("-------> objeto convertido em json " + acao2)
     }
 }
